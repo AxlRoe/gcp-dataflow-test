78bd438481cbcdf3f1997091bf1b1e00520db967
matteo <matteo.marini98@gmail.com>
2021-10-08 17:18:16 +0200

    test

--- kafka-to-bigq/python/build.sh
+++ kafka-to-bigq/python/build.sh
@@ -3,10 +3,10 @@
 KAFKA_ADDRESS=$(gcloud compute instances describe kafka-1-kafka-vm-0 --zone=europe-west6-a --format="yaml(networkInterfaces)" | grep natIP | awk '{print $2}')
 
 TEMPLATE_NAME=Word_Count
-export REGION="europe-west6-a"
-export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/data-flow-sa.json
-export BUCKET_NAME=data-flow-bucket_1
-export PROJECT=data-flow-test-327119
+REGION="europe-west6-a"
+export GOOGLE_APPLICATION_CREDENTIAL=$(pwd)/data-flow-sa.json
+BUCKET_NAME=data-flow-bucket_1
+PROJECT=data-flow-test-327119
 
 #export TEMPLATE_IMAGE="europe-west6-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest"
 # Build the image into Container Registry, this is roughly equivalent to:
@@ -21,20 +21,20 @@ pip install -U -r requirements.txt
 
 #export TEMPLATE_PATH="gs://$BUCKET_NAME/streaming-beam.json"
 
-python -m data-flow-test.word_count \
+python3 -m wordcount \
     --runner DataflowRunner \
-    --project PROJECT \
-    --staging_location gs://BUCKET_NAME/staging \
-    --temp_location gs://BUCKET_NAME/temp \
-    --template_location gs://BUCKET_NAME/templates/$TEMPLATE_NAME \
-    --region ${REGION}
+    --project $PROJECT \
+    --staging_location gs://$BUCKET_NAME/staging \
+    --temp_location gs://$BUCKET_NAME/temp \
+    --template_location gs://$BUCKET_NAME/templates/$TEMPLATE_NAME \
+    --region $REGION
 
-python main.py \
-  --project <your-gcp-project> \
+python3 main.py \
+  --project $PROJECT \
   --job wordcount-$(date +'%Y%m%d-%H%M%S') \
-  --template gs://${BUCKET_NAME}/templates/$TEMPLATE_NAME \
+  --template gs://$BUCKET_NAME/templates/$TEMPLATE_NAME \
   --inputFile gs://apache-beam-samples/shakespeare/kinglear.txt \
-  --output gs://${BUCKET_NAME}/wordcount/outputs
+  --output gs://$BUCKET_NAME/wordcount/outputs
 
 #gcloud dataflow flex-template build $TEMPLATE_PATH \
 #  --image "$TEMPLATE_IMAGE" \
