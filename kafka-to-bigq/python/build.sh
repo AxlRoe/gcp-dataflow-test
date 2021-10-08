03d1d109058d362d940018731965b8bfc3fc557d
mmarini <Matteo_Marini@bmc.com>
2021-10-08 16:36:53 +0200

    test

--- kafka-to-bigq/python/build.sh
+++ kafka-to-bigq/python/build.sh
@@ -2,30 +2,50 @@
 
 KAFKA_ADDRESS=$(gcloud compute instances describe kafka-1-kafka-vm-0 --zone=europe-west6-a --format="yaml(networkInterfaces)" | grep natIP | awk '{print $2}')
 
+TEMPLATE_NAME=Word_Count
+export REGION="europe-west6-a"
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/data-flow-sa.json
 export BUCKET_NAME=data-flow-bucket_1
 export PROJECT=data-flow-test-327119
-export REPOSITORY=dataflow-repo
-export IMAGE_NAME=ktbq-python
 
-export TEMPLATE_IMAGE="europe-west6-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest"
+#export TEMPLATE_IMAGE="europe-west6-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest"
 # Build the image into Container Registry, this is roughly equivalent to:
 #   gcloud auth configure-docker
 #   docker image build -t $TEMPLATE_IMAGE .
 #   docker push $TEMPLATE_IMAGE
-gcloud builds submit --tag "$TEMPLATE_IMAGE" .
+#gcloud builds submit --tag "$TEMPLATE_IMAGE" .
 
-export TEMPLATE_PATH="gs://$BUCKET_NAME/streaming-beam.json"
-gcloud dataflow flex-template build $TEMPLATE_PATH \
-  --image "$TEMPLATE_IMAGE" \
-  --sdk-language "PYTHON" \
-  --metadata-file "metadata.json"
+virtualenv env
+source env/bin/activate
+pip install -U -r requirements.txt
 
-export REGION="europe-west6-a"
+#export TEMPLATE_PATH="gs://$BUCKET_NAME/streaming-beam.json"
 
-# Run the Flex Template.
-gcloud dataflow flex-template run "streaming-beam-`date +%Y%m%d-%H%M%S`" \
-    --template-file-gcs-location "$TEMPLATE_PATH" \
-    --parameters input_subscription="projects/$PROJECT/subscriptions/$SUBSCRIPTION" \
-    --parameters output_table="$PROJECT:$DATASET.$TABLE" \
-    --region "$REGION"
+python -m data-flow-test.word_count \
+    --runner DataflowRunner \
+    --project PROJECT \
+    --staging_location gs://BUCKET_NAME/staging \
+    --temp_location gs://BUCKET_NAME/temp \
+    --template_location gs://BUCKET_NAME/templates/$TEMPLATE_NAME \
+    --region ${REGION}
+
+python main.py \
+  --project <your-gcp-project> \
+  --job wordcount-$(date +'%Y%m%d-%H%M%S') \
+  --template gs://${BUCKET_NAME}/templates/$TEMPLATE_NAME \
+  --inputFile gs://apache-beam-samples/shakespeare/kinglear.txt \
+  --output gs://${BUCKET_NAME}/wordcount/outputs
+
+#gcloud dataflow flex-template build $TEMPLATE_PATH \
+#  --image "$TEMPLATE_IMAGE" \
+#  --sdk-language "PYTHON" \
+#  --metadata-file "metadata.json"
+#
+#export REGION="europe-west6-a"
+#
+## Run the Flex Template.
+#gcloud dataflow flex-template run "streaming-beam-`date +%Y%m%d-%H%M%S`" \
+#    --template-file-gcs-location "$TEMPLATE_PATH" \
+#    --parameters input_subscription="projects/$PROJECT/subscriptions/$SUBSCRIPTION" \
+#    --parameters output_table="$PROJECT:$DATASET.$TABLE" \
+#    --region "$REGION"
