1141a8e2dbbf3339d5c51f7df1b28a868e1c82ff
matteo <matteo.marini98@gmail.com>
2021-10-08 11:25:30 +0200

    test

---
+++ kafka-to-bigq/python/create_sa.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gcloud iam service-accounts delete data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com
+gcloud iam service-accounts create data-flow-sa
+gcloud projects add-iam-policy-binding data-flow-test-327119 --member="serviceAccount:data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com" --role="roles/owner"
+gcloud projects add-iam-policy-binding data-flow-test-327119 --member="serviceAccount:data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com" --role roles/storage.admin 
+gcloud projects add-iam-policy-binding data-flow-test-327119 --member="serviceAccount:data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com" --role roles/artifactregistry.repoAdmin
+gcloud projects add-iam-policy-binding data-flow-test-327119 --member="serviceAccount:data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com" --role roles/artifactregistry.reader
+
+gcloud iam service-accounts keys create data-flow-sa.json --iam-account=data-flow-sa@data-flow-test-327119.iam.gserviceaccount.com
