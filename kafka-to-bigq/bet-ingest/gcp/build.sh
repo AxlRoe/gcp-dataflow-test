3e3aed7b506309545a786d2da7de3538bcfd658b
mmarini <Matteo_Marini@bmc.com>
2022-06-09 18:27:35 +0200

    deploy pipeline to gcp

--- kafka-to-bigq/bet-ingest/gcp/build.sh
+++ kafka-to-bigq/bet-ingest/gcp/build.sh
@@ -7,14 +7,14 @@ PROJECT=scraper-v1-351921
 virtualenv venv
 source venv/bin/activate
 
-python pipeline_gcp.py \
+python prepare_main.py \
 --job data-preparation \
+--project $PROJECT \
 --bucket $BUCKET_NAME \
 --region europe-west1 \
+--setup_file ./setup.py \
 --runner DataflowRunner \
+--staging_location gs://$BUCKET_NAME/staging \
 --requirements_file requirements.txt \
---project $PROJECT \
 --temp_location gs://dump-bucket-4/tmp/ \
---experiment use_unsupported_python_version pipeline \
---staging_location gs://$BUCKET_NAME/staging
-
+--experiment use_unsupported_python_version pipeline
