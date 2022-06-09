b6f96bd2bfd7927dcc199bd91f7e12ee3bd01b58
mmarini <Matteo_Marini@bmc.com>
2022-06-09 18:38:07 +0200

    deploy pipeline to gcp

--- kafka-to-bigq/bet-ingest/gcp/build.sh
+++ kafka-to-bigq/bet-ingest/gcp/build.sh
@@ -4,8 +4,8 @@ export GOOGLE_APPLICATION_CREDENTIAL=data-flow-sa.json
 BUCKET_NAME=dump-bucket-4
 PROJECT=scraper-v1-351921
 
-virtualenv venv
-source venv/bin/activate
+#virtualenv venv
+#source venv/bin/activate
 
 python prepare_main.py \
 --job data-preparation \
