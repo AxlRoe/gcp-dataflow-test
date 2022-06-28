92cc585a95e91c00835d7fe42d165101f14637ea
mmarini <Matteo_Marini@bmc.com>
2022-06-28 16:30:13 +0200

    deploy pipeline to gcp

---
+++ kafka-to-bigq/bet-ingest/dag/dag_ml.py
@@ -0,0 +1,83 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Example Airflow DAG for Google Cloud Dataflow service
+"""
+import os
+from datetime import datetime
+from urllib.parse import urlparse
+
+from airflow import models
+from airflow.providers.apache.beam.operators.beam import BeamRunPythonPipelineOperator
+
+
+START_DATE = datetime(2021, 1, 1)
+
+GCS_TMP = os.environ.get('GCP_DATAFLOW_GCS_TMP', 'gs://dump-bucket-4/temp/')
+GCS_STAGING = os.environ.get('GCP_DATAFLOW_GCS_STAGING', 'gs://dump-bucket-4/staging/')
+GCS_OUTPUT = os.environ.get('GCP_DATAFLOW_GCS_OUTPUT', 'gs://dump-bucket-4/output')
+GCS_PYTHON = os.environ.get('GCP_DATAFLOW_PYTHON', 'gs://dump-bucket-4/wordcount_debugging.py')
+
+GCS_JAR_PARTS = urlparse(GCS_JAR)
+GCS_JAR_BUCKET_NAME = GCS_JAR_PARTS.netloc
+GCS_JAR_OBJECT_NAME = GCS_JAR_PARTS.path[1:]
+
+default_args = {
+    'dataflow_default_options': {
+        'tempLocation': GCS_TMP,
+        'stagingLocation': GCS_STAGING,
+    }
+}
+
+with models.DAG(
+    "example_gcp_dataflow_native_python",
+    default_args=default_args,
+    start_date=START_DATE,
+    catchup=False,
+    schedule_interval='@once',  # Override to match your needs
+    tags=['example'],
+) as dag_native_python:
+
+    # # [START howto_operator_start_python_job]
+    # start_python_job = BeamRunPythonPipelineOperator(
+    #     task_id="start-python-job",
+    #     py_file=GCS_PYTHON,
+    #     py_options=[],
+    #     pipeline_options={
+    #         'output': GCS_OUTPUT,
+    #     },
+    #     py_requirements=['apache-beam[gcp]==2.39.0'],
+    #     py_interpreter='python3',
+    #     py_system_site_packages=False,
+    #     dataflow_config={'location': 'europe-west1'},
+    # )
+
+    start_python_job_local = BeamRunPythonPipelineOperator(
+        task_id="start-python-job-local",
+        py_file='apache_beam.examples.wordcount',
+        py_options=['-m'],
+        pipeline_options={
+            'output': GCS_OUTPUT,
+        },
+        py_requirements=['apache-beam[gcp]==2.14.0'],
+        py_interpreter='python3',
+        py_system_site_packages=False,
+    )
+
+
---
+++ kafka-to-bigq/bet-ingest/dag/p_test.py
@@ -0,0 +1,65 @@
+"""A word-counting workflow."""
+
+from __future__ import absolute_import
+
+import argparse
+import logging
+from datetime import datetime, time
+
+import apache_beam as beam
+from apache_beam import DoFn, ParDo
+from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.options.pipeline_options import PipelineOptions
+
+class MatchRow (DoFn):
+    def process(self, element):
+        # event_id, competition_id, cutoff_date, delta, event_name, favourite, guest, home, score = element.split(";")
+        return [{
+            'event_id': element['event_id'],
+            'competition_id': element['competition_id'],
+            'cutoff_date': element['cutoff_date'],
+            'event_name': element['event_name'],
+            'guest': element['guest'],
+            'home': element['home'],
+            'favourite': element['favourite'],
+            'score': element['outcome']
+        }]
+
+def run(args=None):
+    """Main entry point; defines and runs the wordcount pipeline."""
+    # Set `save_main_session` to True so DoFns can access globally imported modules.
+    pipeline_options = PipelineOptions(
+        args, save_main_session=True
+    )
+
+    start_of_day = datetime.combine(datetime.utcnow(), time.min)
+    start_of_day = start_of_day.strftime("%Y-%m-%d") #'2022-06-27'
+    bucket = 'dump-bucket-4'
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+
+        match_table_spec = bigquery.TableReference(projectId='scraper-v1-351921', datasetId='bet', tableId='match')
+        match_dict = (
+                pipeline
+                # Each row is a dictionary where the keys are the BigQuery columns
+                | 'Read match bq table' >> beam.io.ReadFromBigQuery(gcs_location='gs://' + bucket + '/tmp/', table=match_table_spec)
+                | "Convert list in row " >> ParDo(MatchRow())
+                | "Filter matches without favourite" >> beam.Filter(lambda row: row['favourite'] is not None)
+                #| "debug match " >> beam.Map(print)
+        )
+
+    logging.info("pipeline started")
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument(
+    #     '--bucket',
+    #     dest='bucket',
+    #     required=True,
+    #     help='bucket where read/write csv'
+    # )
+
+    known_args, pipeline_args = parser.parse_known_args()
+    run(pipeline_args)
