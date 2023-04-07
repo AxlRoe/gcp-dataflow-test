6630afc49514ff72d5c5c92d2393583732d82dd0
mmarini <Matteo_Marini@bmc.com>
2023-04-07 17:09:49 +0200

    test

--- pubsub-to-bigq/setup_project.sh
+++ pubsub-to-bigq/setup_project.sh
@@ -2,7 +2,7 @@
 
 gcloud config set project scraper-vx3
 
-APIS=$(gcloud services list --available | grep -E -i '^container\.|^cloudfunction|dataflow|^stackdriver|^storage\.google|storage-api|^bigquery\.|^pubsub\.|datastore|cloudresource|^compute\.' | awk '{print $1}')
+APIS=$(gcloud services list --available | awk '{print $NF}' | grep -E -i '^cloudbuild\.|^sourcerepo\.|^container\.|^cloudfunction|dataflow|^stackdriver|^storage\.google|storage-api|^bigquery\.|^pubsub\.|datastore|cloudresource|^compute\.')
 
 for api in $APIS; do
   echo "Enabling api " $api
